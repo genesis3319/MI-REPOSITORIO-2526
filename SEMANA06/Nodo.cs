bb1863d8d48cf8ce4a64150323acacfcf9fd0d31
Genesis Nikolle Andy Chimbo <gn.andyc@uea.edu.ec>
2026-01-25 01:16:46 -0500

    semana06
    semana06

---
+++ SEMANA06/Nodo.cs
@@ -0,0 +1,12 @@
+// Clase Nodo: representa un elemento de la lista enlazada
+public class Nodo
+{
+    public int Data;
+    public Nodo? Next;
+
+    public Nodo(int data)
+    {
+        Data = data;
+        Next = null;
+    }
+}
---
+++ SEMANA06/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //creamos una lista enlazada
+        ListadoSimple lista = new ListadoSimple();
+
+        //insertamos elementos
+        lista.InsertarFinal(10);
+        lista.InsertarFinal(15);
+        lista.InsertarFinal(20);
+        lista.InsertarFinal(25);
+        lista.InsertarFinal(30);
+        lista.InsertarFinal(35);
+        lista.InsertarFinal(40);
+
+        //lista original
+        Console.WriteLine("Lista original:");
+        lista.MostrarLista();
+
+        Console.WriteLine("\nNúmero de elementos: " + lista.ContarElementos());
+
+        //invertir lista
+        lista.InvertirLista();
+
+        //mostramos lista invertida
+        Console.WriteLine("\nLista invertida:");
+        lista.MostrarLista();
+    }
+}
