c81987152d498613037d61907cd145ab379a1828
Genesis Nikolle Andy Chimbo <gn.andyc@uea.edu.ec>
2026-01-25 01:14:17 -0500

    semana07
    semana07

---
+++ TAREA_PILAS/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese una expresión matemática: ");
+        string expresion = Console.ReadLine();
+
+        Stack<char> pila = new Stack<char>();
+        bool balanceada = true;
+
+        foreach (char c in expresion)
+        {
+            // 1) Si es símbolo de apertura → Push
+            if (c == '(' || c == '{' || c == '[')
+            {
+                pila.Push(c);
+            }
+            // 2) Si es símbolo de cierre
+            else if (c == ')' || c == '}' || c == ']')
+            {
+                // Si la pila está vacía → error
+                if (pila.Count == 0)
+                {
+                    balanceada = false;
+                    break;
+                }
+
+                // Sacar el último símbolo abierto
+                char ultimo = pila.Pop();
+
+                // Verificar que coincida
+                if ((ultimo == '(' && c != ')') ||
+                    (ultimo == '{' && c != '}') ||
+                    (ultimo == '[' && c != ']'))
+                {
+                    balanceada = false;
+                    break;
+                }
+            }
+        }
+
+        // Si quedó algo en la pila → no está balanceada
+        if (pila.Count != 0)
+            balanceada = false;
+
+        if (balanceada)
+            Console.WriteLine("Fórmula balanceada.");
+        else
+            Console.WriteLine("Fórmula NO balanceada.");
+    }
+}
---
+++ TorresHanoi/Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+class Program
+{
+    static Stack<int> origen = new Stack<int>();
+    static Stack<int> auxiliar = new Stack<int>();
+    static Stack<int> destino = new Stack<int>();
+
+    static void Main()
+    {
+        int discos = 3;
+
+        for (int i = discos; i >= 1; i--)
+        {
+            origen.Push(i);
+        }
+
+        ResolverHanoi(discos, origen, destino, auxiliar, "Origen", "Destino", "Auxiliar");
+    }
+
+    static void ResolverHanoi(
+        int n,
+        Stack<int> origen,
+        Stack<int> destino,
+        Stack<int> auxiliar,
+        string nombreOrigen,
+        string nombreDestino,
+        string nombreAuxiliar)
+    {
+        if (n == 0) return;
+
+        ResolverHanoi(n - 1, origen, auxiliar, destino, nombreOrigen, nombreAuxiliar, nombreDestino);
+
+        int disco = origen.Pop();
+        destino.Push(disco);
+        Console.WriteLine($"Movimiento de disco {disco} de {nombreOrigen} a {nombreDestino}");
+
+        ResolverHanoi(n - 1, auxiliar, destino, origen, nombreAuxiliar, nombreDestino, nombreOrigen);
+    }
+}
+
+
